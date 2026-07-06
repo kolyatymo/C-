84a130a050746132e7c452d548bc3ebf3d51f53c
Kolya Tymoshchuk <kolya.tymo@gmail.com>
2026-07-06 21:07:00 +0300

    task

---
+++ 12_task/Management.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_task
+{
+    internal class Management
+    {
+        Dictionary<string, string> management = new Dictionary<string, string>()
+        {
+            ["opGmail.com"] = "OPgsayuc214",
+            ["JHjaadGmail.com"] = "t35gsayuc214"
+        };
+
+        public void AddTomanagement()
+        {
+            Console.WriteLine("Enetr Login");
+            string login = Console.ReadLine();
+            Console.WriteLine("Enetr PAsword");
+            string pasword = Console.ReadLine();
+            if (management.ContainsKey(login))
+                Console.WriteLine($"login --> {login} already exists");
+            else
+                management.Add(login, pasword);
+        }
+
+        public void RemoveLogin()
+        {
+            Console.WriteLine("Enetr Login for delete ");
+            string login = Console.ReadLine();
+            if(management.ContainsKey(login))
+                management.Remove(login);
+            else
+                Console.WriteLine($"login --> {login} already exists");
+        }
+        public void ChangeInformation()
+        {
+            Console.WriteLine("Enter the login and password you want to change");
+            Console.WriteLine("Enetr Login");
+            string login = Console.ReadLine();
+            Console.WriteLine("Enetr PAsword");
+            string pasword = Console.ReadLine();
+            management.Remove(login);
+
+            Console.WriteLine("Eneter new login and pasword");
+            Console.WriteLine("Enetr Login");
+            string log = Console.ReadLine();
+            Console.WriteLine("Enetr PAsword");
+            string pas = Console.ReadLine();
+            management.Add(log, pas);
+        }
+
+        public void PrintInfo(string login)
+        {
+            foreach (var item in management.Keys)
+            {
+                if (management.ContainsKey(login))
+                    Console.WriteLine($"{item,-10} {management[item]}");
+                else
+                    Console.WriteLine("The login does not exist");
+            }
+        }
+        public void Print()
+        {
+            foreach (var item in management)
+            {
+                Console.WriteLine($"Login --> {item.Key,-10}Pasword --> {item.Value}");
+            }
+        }
+
+    }
+}
---
+++ 12_task/Program.cs
@@ -0,0 +1,51 @@
+﻿using _12_task;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Management management = new Management();
+
+        while (true)
+        {
+            Console.WriteLine("\n__________________________________\n");
+            Console.WriteLine("1 --> Add employee");
+            Console.WriteLine("2 --> Remove employee");
+            Console.WriteLine("3 --> Change login/password");
+            Console.WriteLine("4 --> Find password by login");
+            Console.WriteLine("5 --> Show all employees");
+            Console.WriteLine("0 --> Exit");
+            Console.Write("Choose --> ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    management.AddTomanagement();
+                    break;
+
+                case "2":
+                    management.RemoveLogin();
+                    break;
+
+                case "3":
+                    management.ChangeInformation();
+                    break;
+
+                case "4":
+                    Console.Write("Enter login --> ");
+                    string login = Console.ReadLine();
+                    management.PrintInfo(login);
+                    break;
+
+                case "5":
+                    management.Print();
+                    break;
+
+                case "0":
+                    return;
+            }
+        }
+    }
+}
