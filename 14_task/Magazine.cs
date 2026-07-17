eeb817aee4421ece14e6f8e285fee1bd39f47086
Kolya Tymoshchuk <kolya.tymo@gmail.com>
2026-07-17 20:29:31 +0300

    tasks

---
+++ 14_task/Magazine.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace _14_task
+{
+    public class Magazine
+    {
+        public string Name { get; set; }
+        public string NamePublishing{ get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int Pages { get; set; }
+
+        public void FillMagazine()
+        {
+            Console.WriteLine($"Enter name magazine ");
+            Name = Console.ReadLine();
+            Console.WriteLine($"Enter namePublishing ");
+            NamePublishing = Console.ReadLine();
+            Console.WriteLine($"Enter Date ");
+            Date = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine($"Enter count Pages ");
+            Pages = int.Parse(Console.ReadLine());
+        }
+        public void PrintMagazine()
+        {
+            Console.WriteLine($"Name magazine --> {Name}");
+            Console.WriteLine($"Name Publishe --> {NamePublishing}");
+            Console.WriteLine($"Date          --> {Date}");
+            Console.WriteLine($"Count Pages   --> {Pages}");
+        }
+
+        public void SerializationMagazine()
+        {
+            string Fname = "Magazine.xml";
+
+            XmlSerializer xs = new XmlSerializer(typeof(Magazine));
+            using(FileStream fs = new FileStream(Fname, FileMode.Create))
+            {
+                xs.Serialize(fs, this);
+            }
+
+        }
+
+        public void DeserializationMagazine()
+        {
+            string Fname = "Magazine.xml";
+            XmlSerializer xsL = new XmlSerializer(typeof(Magazine));
+
+
+            using (TextReader tr = new StreamReader(Fname))
+            {
+                var magazine = xsL.Deserialize(tr) as Magazine;
+
+                Console.WriteLine($"Recoverd");
+
+                magazine.PrintMagazine();
+            }
+        }
+    }
+}
---
+++ 14_task/Program.cs
@@ -0,0 +1,47 @@
+﻿using _14_task;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Magazine magazine = new Magazine();
+
+        while (true)
+        {
+            Console.WriteLine("______________ Menu ______________");
+            Console.WriteLine("1 --> Enter magazine information");
+            Console.WriteLine("2 --> Print magazine information");
+            Console.WriteLine("3 --> Save magazine (Serialization)");
+            Console.WriteLine("4 --> Load magazine (Deserialization)");
+            Console.WriteLine("0 --> Exit");
+
+            Console.Write("Choose --> ");
+            int choice = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            switch (choice)
+            {
+                case 1:
+                    magazine.FillMagazine();
+                    break;
+
+                case 2:
+                    magazine.PrintMagazine();
+                    break;
+
+                case 3:
+                    magazine.SerializationMagazine();
+                    break;
+
+                case 4:
+                    magazine.DeserializationMagazine();
+                    break;
+
+                case 0:
+                    Console.WriteLine("Bye (-_-)");
+                    return;
+            }
+        }
+    }
+}
