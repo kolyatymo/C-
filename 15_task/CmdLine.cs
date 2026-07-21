b78088d9a94a708480398aaa41a39842404dde38
Kolya Tymoshchuk <kolya.tymo@gmail.com>
2026-07-21 13:46:56 +0300

    task

---
+++ 15_task/CmdLine.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace _15_task
+{
+    internal class CmdLine
+    {
+        public void md(string foldername)
+        {
+            if(!Directory.Exists(foldername))
+            {
+                Directory.CreateDirectory(foldername);
+                Console.WriteLine($"Folder '{foldername}' was created");
+            }
+            else
+            {
+                Console.WriteLine("Eroor");
+            }
+        }
+
+        public void rd(string foldername)
+        {
+            if(Directory.Exists(foldername))
+            {
+                Directory.Delete(foldername,true);
+                Console.WriteLine($"Folder '{foldername}' was deleted \n\n");
+            }
+            else
+            {
+                Console.WriteLine("Eroor");
+            }
+        }
+
+        public void cd(string foldername)
+        {
+            if (Directory.Exists(foldername))
+            {
+                Directory.SetCurrentDirectory(foldername);
+                Console.WriteLine($"Folder '{foldername}'");
+            }
+            else
+            {
+                Console.WriteLine("Eroor");
+            }
+        }
+        public void dir(string patern = "*")
+        {
+            Console.WriteLine("Folders");
+            foreach (var folder in Directory.GetDirectories(Directory.GetCurrentDirectory(), patern))
+            {
+                Console.WriteLine(Path.GetFileName(folder)); 
+            }
+            Console.WriteLine("File");
+            foreach (var file in Directory.GetFiles(Directory.GetCurrentDirectory(),patern))
+            {
+                Console.WriteLine(Path.GetFileName(file));
+            }
+        }
+
+        public void create(string fname)
+        {
+
+            Console.Write($"Enter text for file '{fname}'");
+            string text = Console.ReadLine();
+
+            File.WriteAllText(fname, text);
+
+            Console.WriteLine($"File '{fname}' was created");
+        }
+        public void type(string fname)
+        {
+            if(File.Exists(fname))
+            {
+                Console.WriteLine(File.ReadAllText(fname));
+            }
+            else
+            {
+                Console.WriteLine("Eroor");
+            }
+        }
+
+        public void copy(string fname, string fcopy)
+        {
+            if(File.Exists(fname))
+            {
+                File.Copy(fname, fcopy,true);
+            }
+            else
+            {
+                Console.WriteLine("Eroor");
+            }
+        }
+
+        public void del(string fname)
+        {
+            if (File.Exists(fname))
+            {
+                File.Delete(fname);
+                Console.WriteLine($"File '{fname}' wad deleted");
+            }
+            else
+            {
+                Console.WriteLine("Eroor");
+            }
+        }
+
+        public void append(string fname)
+        {
+            if (File.Exists(fname))
+            {
+                Console.WriteLine($"Enter info in File '{fname}'");
+                var text = Console.ReadLine();
+                File.AppendAllText(fname, text);
+            }
+            else
+            {
+                Console.WriteLine("Eroor");
+            }
+        }
+    }   
+}
---
+++ 15_task/Program.cs
@@ -0,0 +1,91 @@
+﻿using _15_task;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CmdLine cmd = new CmdLine();
+
+        while (true)
+        {
+            Console.WriteLine("\n____________ MENU ___________");
+            Console.WriteLine("1 --> Create folder (md)");
+            Console.WriteLine("2 --> Delete folder (rd)");
+            Console.WriteLine("3 --> Change current folder (cd)");
+            Console.WriteLine("4 --> Show files and folders (dir)");
+            Console.WriteLine("5 --> Create file (create)");
+            Console.WriteLine("6 --> Show file content (type)");
+            Console.WriteLine("7 --> Copy file (copy)");
+            Console.WriteLine("8 --> Delete file (del)");
+            Console.WriteLine("9 --> Append text to file (append)");
+            Console.WriteLine("0 --> Exit");
+            Console.Write("Choose --> ");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Enter folder name --> ");
+                    cmd.md(Console.ReadLine());
+                    break;
+
+                case 2:
+                    Console.Write("Enter folder name --> ");
+                    cmd.rd(Console.ReadLine());
+                    break;
+
+                case 3:
+                    Console.Write("Enter folder name --> ");
+                    cmd.cd(Console.ReadLine());
+                    break;
+
+                case 4:
+                    Console.Write("Enter pattern (* for all) --> ");
+                    string pattern = Console.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(pattern))
+                        cmd.dir();
+                    else
+                        cmd.dir(pattern);
+                    break;
+
+                case 5:
+                    Console.Write("Enter file name --> ");
+                    cmd.create(Console.ReadLine());
+                    break;
+
+                case 6:
+                    Console.Write("Enter file name --> ");
+                    cmd.type(Console.ReadLine());
+                    break;
+
+                case 7:
+                    Console.Write("Enter source file --> ");
+                    string source = Console.ReadLine();
+
+                    Console.Write("Enter destination file --> ");
+                    string destination = Console.ReadLine();
+
+                    cmd.copy(source, destination);
+                    break;
+
+                case 8:
+                    Console.Write("Enter file name --> ");
+                    cmd.del(Console.ReadLine());
+                    break;
+
+                case 9:
+                    Console.Write("Enter file name --> ");
+                    cmd.append(Console.ReadLine());
+                    break;
+
+                case 0:
+                    Console.WriteLine("Bye (-_-)");
+                    return;
+            }
+        }
+    }
+}
